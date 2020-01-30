81011306453aae495613a6eceee429cf229cac58
Alexander Goncearenco <alexander.goncearenco@gmail.com>
2020-01-29 19:17:01 -0500

    simplified menu for profiles - no need to say profile calculate changed static class to object method

--- mutagene/cli/profile_menu.py
+++ mutagene/cli/profile_menu.py
@@ -11,22 +11,19 @@ genome_error_message = """requires genome name argument -g hg19, hg38, mm10, see
 
 class ProfileMenu(object):
     def __init__(self, parser):
-        parser.add_argument('action', choices=['calculate'])  # , 'plot', 'fingerprint', 'compare'])
+        # parser.add_argument('action', choices=['calculate'])  # , 'plot', 'fingerprint', 'compare'])
         parser.add_argument("--infile", "-i", nargs='*', help="Input file format", type=argparse.FileType('r'))
         parser.add_argument('--outfile', "-o", nargs='?', type=argparse.FileType('w'), default=sys.stdout,
                             help="Name of output file, will be generated in TSV format")
         parser.add_argument('--genome', "-g", help="Location of genome assembly file", type=str)
         parser.add_argument('--input-format', "-f", help="Input format: auto, MAF, VCF", type=str, default='auto')
 
-    @classmethod
-    def callback(cls, args):
-        print('ProfileMenu', args.action)
-        getattr(cls, args.action)(args)
+    def callback(self, args):
+        # print('ProfileMenu', args.action)
+        self.calculate(args)
 
-    @classmethod
-    def calculate(cls, args):
-        print("Calculating...")
-
+    def calculate(self, args):
+        # print("Calculating...")
         if not args.infile:
             logger.warning("Provide input file in VCF or MAF format (-i) and a corresponding genome assembly (-g)")
             return
@@ -34,5 +31,3 @@ class ProfileMenu(object):
             logger.warning(genome_error_message)
             return
         calc_profile(args.infile, args.outfile, args.genome, args.input_format)
-
-################################################
